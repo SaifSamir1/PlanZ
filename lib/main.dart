8f6b22efae2a1c030304927b334bef1f72003393
Walid Atef <walidatef@gmail.com>
2025-09-10 18:16:26 +0300

    oowner

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:plan_z/features/owner_features/owner_ui/screens/navigation_screen.dart';
 import 'package:plan_z/notification_screen.dart';
 import 'package:plan_z/vendor_home_screen.dart';
+import 'features/new_owner_features/screens/navigation_screen.dart';
 
 void main() {
   runApp(const PlanZ());
