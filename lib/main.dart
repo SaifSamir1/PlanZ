819164c48cd6e17ccb0ce1096ede53f531c7e226
Hoor <hoormahmoud000@gmail.com>
2025-09-19 14:55:33 +0300

    finish_profile_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
+import 'package:plan_z/features/auth/ui/screens/profile_screen.dart';
 import 'package:plan_z/features/new_owner_features/chat_bot/cubits/chat_cubit.dart';
 import 'package:plan_z/features/new_owner_features/create_event_screen/ui/screens/create_event_screen.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  
+
   Intl.defaultLocale = 'ar';
-  
+
   runApp(const PlanZ());
 }
 
@@ -20,12 +19,13 @@ class PlanZ extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PlanZ Chat',
+      home: ProfileScreen(),
+      // title: 'PlanZ Chat',
       debugShowCheckedModeBanner: false,
-      home: BlocProvider(
-        create: (context) => ChatCubit(),
-        child: const CreateEventScreen(),
-      ),
+      // home: BlocProvider(
+      //   create: (context) => ChatCubit(),
+      //   child: const CreateEventScreen(),
+      // ),
     );
   }
 }
